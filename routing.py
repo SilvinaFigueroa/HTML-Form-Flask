dfcd7be0eda6188635f0f2281770c3bfe5eff597
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-04-16 13:23:21 -0700

    Adding fortune results

--- routing.py
+++ routing.py
@@ -2,6 +2,8 @@
 # An object of Flask class is our WSGI application.
 from flask import Flask, redirect, url_for, request
 
+import fortune_results
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -29,6 +31,30 @@ def login():
     else:
         user = request.args.get('nm')
         return redirect(url_for('success', name=user))
+
+
+@app.route('/Fortune', methods=['POST', 'GET'])
+def fortune():
+    if request.method == 'POST':
+        user_name = request.form['user']
+        user_color = request.form['color']
+        user_number = request.form['number']
+
+        return redirect(url_for('user_fortune', user=user_name, color=user_color, number=user_number))
+
+    else:
+
+        user_name = request.args.get('user')
+        user_color = request.args.get('color')
+        user_number = request.args.get('number')
+        return redirect(url_for('user_fortune', user=user_name, color=user_color, number=user_number))
+
+
+@app.route('/YourFortune/<user>/<color>/<number>')
+def user_fortune(user, color, number):
+    # return 'Your Fortune: {} {} {}'.format(user, color, number)
+    response = fortune_results.results(user, color, number)
+    return response
 
 
 # main driver function
